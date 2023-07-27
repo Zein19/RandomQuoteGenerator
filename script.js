ce0eb3ec0def9f5bc179e797ea400331d315860d
Zein19 <zein.23@hotmail.co.uk>
2023-07-27 13:01:31 +0100

    Made app functioning

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assign variables
+const quote = document.querySelector('h2')
+const btn = document.getElementById('btn')
+const author = document.querySelector('h3')
+
+const url = "https://api.quotable.io/random";
+
+
+ let  newQuote = () =>{
+    fetch(url)
+    .then((data) => data.json())
+    .then((item) =>{
+        quote.innerText = item.content;
+        author.innerText = item.author;
+    });
+ };
+    
+window.addEventListener('load', newQuote)
+btn.addEventListener('click', newQuote)
